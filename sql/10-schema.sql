796124b7b32e2e1210ad72c1bdd233136d3373f0
Javier Arias <javier@arias.re>
2019-04-24 12:05:46 +0100

    remove canonical from index

--- sql/10-schema.sql
+++ sql/10-schema.sql
@@ -46,6 +46,6 @@ CREATE TABLE work_uri(
 );
 CREATE UNIQUE INDEX canonical_uri ON work_uri(work_id, uri_scheme) WHERE canonical = TRUE;
 
-CREATE INDEX identifiers ON work_uri(uri_scheme, uri_value, canonical);
+CREATE INDEX identifiers ON work_uri(uri_scheme, uri_value);
 CREATE INDEX lower_titles ON work_title((lower(title)));
 CREATE INDEX title_length ON work_title((length(title)));
